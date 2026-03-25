b5d082f90483c6ea702e90fa16d7ec68ab3fe34e
Srikanth s <senthilsri1106@gmail.com>
2026-03-25 09:19:00 +0530

    footer quick fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
     <footer className="footer">
@@ -10,10 +12,10 @@ export default function Footer() {
         <div className="footer-links">
           <h4>Quick Links</h4>
           <ul>
-            <li><a href="/events">Events</a></li>
-            <li><a href="/schedule">Schedule</a></li>
-            <li><a href="/points">Points System</a></li>
-            <li><a href="/leaderboard">Leaderboard</a></li>
+            <li><Link to="/events">Events</Link></li>
+            <li><Link to="/schedule">Schedule</Link></li>
+            <li><Link to="/points">Points System</Link></li>
+            <li><Link to="/leaderboard">Leaderboard</Link></li>
           </ul>
         </div>
 
@@ -23,7 +25,7 @@ export default function Footer() {
             <img src="/logo.png" alt="Youth Karada Mangaluru" style={{ height: '32px', width: 'auto', mixBlendMode: 'multiply', filter: 'contrast(1.1)' }} />
           </div>
           <p>Youth Karada Mangaluru</p>
-          <a href="/contact" className="contact-link">Contact Us</a>
+          <Link to="/contact" className="contact-link">Contact Us</Link>
         </div>
       </div>
 
